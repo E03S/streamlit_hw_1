1370c2f25f2f08e75a7aeb63f9f3f54f69313454
E03S <pigalskiy2001@gmail.com>
2023-12-12 22:01:52 +0300

    bins mod

--- streamlit.py
+++ streamlit.py
@@ -10,10 +10,15 @@ data = pd.read_csv('combined.csv')
 
 st.subheader('Гистограма распределения')
 
-column_name = st.selectbox('Выберете признак',('AGREEMENT_RK','TARGET','AGE','SOCSTATUS_WORK_FL','SOCSTATUS_PENS_FL','GENDER',
+column_name = st.selectbox('Выберете признак',('TARGET','AGE','SOCSTATUS_WORK_FL','SOCSTATUS_PENS_FL','GENDER',
 'CHILD_TOTAL','DEPENDANTS','PERSONAL_INCOME','LOAN_NUM_TOTAL','LOAN_NUM_CLOSED'))
 
+if len(data[column_name].unique())>100:
+    bins_hs = 100
+else:
+    bins_hs = len(data[column_name].unique())
+
 fig, ax = plt.subplots()
-ax.hist(data[column_name],bins = len(data[column_name].unique()))
+ax.hist(data[column_name],bins = bins_hs)
 
 st.pyplot(fig)
