ca3440bca2bafdc9da6e60ed6b75c35740738e10
E03S <pigalskiy2001@gmail.com>
2023-12-12 21:29:41 +0300

    pyplot better

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 st.title('Bank analysis')
 st.text('Разведочный анализ данных банка')
@@ -12,7 +13,4 @@ st.subheader('Гистограма распределения')
 column_name = st.selectbox('Выберете признак',('AGREEMENT_RK','TARGET','AGE','SOCSTATUS_WORK_FL','SOCSTATUS_PENS_FL','GENDER',
 'CHILD_TOTAL','DEPENDANTS','PERSONAL_INCOME','LOAN_NUM_TOTAL','LOAN_NUM_CLOSED'))
 
-hist_values = np.histogram(
-    data[column_name], bins=100)
-
-st.bar_chart(hist_values)
+st.pyplot(plt.hist(df_selected[column_name],bins = len(df_selected[column_name].unique())))
